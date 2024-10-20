f1aa3bcccc11ebe6e4b97803b1006f987eb1b0c0
euzhengxi <euzhengxi110@gmail.com>
2024-10-20 00:53:42 -0400

    rust backend

---
+++ counter-app/src/provide_liquidity.rs
@@ -0,0 +1,70 @@
+// provide_liquidity.rs
+use essential_app_utils::inputs::Encode;
+use essential_sign::secp256k1::ecdsa::RecoverableSignature;
+use essential_types::{solution::{Solution, SolutionData}, Word};
+
+use crate::{Query, lp_balance_key};
+
+pub struct Init {
+    pub hashed_key: [Word; 4],
+    pub amount_a: Word,
+    pub amount_b: Word,
+}
+
+pub struct ToSign {
+    pub hashed_key: [Word; 4],
+    pub amount_a: Word,
+    pub amount_b: Word,
+}
+
+pub struct BuildSolution {
+    pub hashed_key: [Word; 4],
+    pub amount_a: Word,
+    pub amount_b: Word,
+    pub signature: RecoverableSignature,
+}
+
+pub fn data_to_sign(init: Init) -> anyhow::Result<ToSign> {
+    Ok(ToSign {
+        hashed_key: init.hashed_key,
+        amount_a: init.amount_a,
+        amount_b: init.amount_b,
+    })
+}
+
+pub fn build_solution(build: BuildSolution) -> anyhow::Result<Solution> {
+    let BuildSolution {
+        hashed_key,
+        amount_a,
+        amount_b,
+        signature,
+    } = build;
+
+    let pub_vars = crate::amm::ProvideLiquidity::PubVars {
+        user: hashed_key,
+        amount_a,
+        amount_b,
+    };
+
+    let signature = signature.encode();
+    let vars = crate::amm::ProvideLiquidity::Vars {
+        auth: crate::amm::LiquidityAuth::Signed(signature),
+    };
+
+    let mutations = crate::amm::storage::mutations()
+        .lp_balances(|map| map.entry(hashed_key, amount_a + amount_b))
+        .token_a_balance(|value| value + amount_a)
+        .token_b_balance(|value| value + amount_b)
+        .total_liquidity(|value| value + amount_a + amount_b);
+
+    let solution = SolutionData {
+        predicate_to_solve: crate::amm::ProvideLiquidity::ADDRESS,
+        decision_variables: vars.into(),
+        transient_data: pub_vars.into(),
+        state_mutations: mutations.into(),
+    };
+
+    Ok(Solution {
+        data: vec![solution],
+    })
+}
